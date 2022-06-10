e1e44a037944eda0532f649fe0cda1dd4418b879
Harlan Pierre <harlan.evron@gmail.com>
2022-06-10 00:50:57 -0300

    Testando a primeira classe de dominio - categoria

---
+++ src/main/java/com/hs/aplicacaohsimportados/domain/Categoria.java
@@ -0,0 +1,56 @@
+package com.hs.aplicacaohsimportados.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		Id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+}
